e2322a1d2c4b313a84b9049c3fc82a92829479a2
semen <bethesda.stillxxx@gmail.com>
2020-06-27 12:46:28 +0300

    Убрал ненужные комментарии

--- Project/Assets/Scripts/CharacterScripts/CameraMover.cs
+++ Project/Assets/Scripts/CharacterScripts/CameraMover.cs
@@ -3,36 +3,14 @@
 public class CameraMover : MonoBehaviour
 {
 	[Range(0f, 2f)] [SerializeField] private float SmoothTime = 0f;
-	//[SerializeField] private float ViewPortFactor = 0f;
 	[SerializeField] private Transform Target = null;
-	//[SerializeField] private Vector3 ViewPortSize;
 
 	private Vector3 Useless;
-	//private Vector2 TargetPosition;
-	//private Vector2 Distance;
-	//private Camera Camera;
-
-	private void Awake()
-	{
-		//Camera = GetComponent<Camera>();
-		//TargetPosition = Target.position;
-	}
 
 	private void LateUpdate()
 	{
 		if (Target != null)
 		{
-			//ViewPortSize = (Camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) - Camera.ScreenToWorldPoint(Vector2.zero)) * ViewPortFactor;
-
-			//Distance = Target.position - transform.position;
-			//if (Mathf.Abs(Distance.x) > ViewPortSize.x / 2)
-			//{
-			//	TargetPosition.x = Target.position.x - (ViewPortSize.x / 2 * Mathf.Sign(Distance.x));
-			//}
-			//if (Mathf.Abs(Distance.y) > ViewPortSize.y / 2)
-			//{
-			//	TargetPosition.y = Target.position.y - (ViewPortSize.y / 2 * Mathf.Sign(Distance.y));
-			//}
 			SmoothMove();
 		}
 	}
