318d5e34b7c830131a8f21a6ccd6cdf90b65ee44
semen <bethesda.stillxxx@gmail.com>
2020-08-14 13:06:58 +0300

    Добавил грифты

---
+++ Project/Assets/Scripts/Core/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+	private void Start()
+	{
+		Load();
+	}
+
+	private void Load()
+	{
+		IOnSceneStartLoading[] forLoad = GetComponents<IOnSceneStartLoading>();
+
+		foreach (var item in forLoad)
+		{
+			item.SceneLoad();
+		}
+	}
+}
---
+++ Project/Assets/Scripts/UI/Fonts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Fonts : MonoBehaviour, IOnSceneStartLoading
+{
+	[SerializeField] private Font Font;
+
+	public void SceneLoad()
+	{
+		var textComponents = FindObjectsOfType<Text>();
+
+		foreach (var item in textComponents)
+		{
+			item.font = Font;
+			item.fontSize = 30;
+		}
+	}
+
+}
