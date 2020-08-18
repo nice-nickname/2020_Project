bd4584d3cb462e0e4f15bf0b65dba45c6a26d010
semen <bethesda.stillxxx@gmail.com>
2020-08-18 20:56:13 +0300

    Добавил классы для создания очереди корутин (для анимаций, заскриптованных событий и тд

---
+++ Project/Assets/Scripts/Coroutines/Coroutine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+class Coroutine
+{
+	public TaskStatus Status { get; private set; }
+
+	private ICoroutinedTask TaskHandler;
+
+	public Coroutine(ICoroutinedTask _class)
+	{
+		TaskHandler = _class;
+		Status = TaskStatus.Waiting;
+	}
+
+	public IEnumerator Method()
+	{
+		Status = TaskStatus.Executed;
+
+		while (true)
+		{
+			if (TaskHandler.Ended() == false)
+			{
+				TaskHandler.Task();
+				yield return null;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		Status = TaskStatus.Finished;
+		yield break;
+	}
+
+
+}
---
+++ Project/Assets/Scripts/Coroutines/CoroutineQueueExecutor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineQueueExecutor : MonoBehaviour
+{
+	private Queue<Coroutine> Tasks;
+	private Coroutine Current;
+
+	private void Awake()
+	{
+		Tasks = new Queue<Coroutine>();
+	}
+
+	void Update()
+	{
+		if (Tasks.Count == 0) return;
+
+		if (Current == null)
+		{
+			Current = Tasks.Dequeue();
+
+			StartCoroutine(Current.Method());
+		}
+		else
+		{
+			if (Current.Status == TaskStatus.Finished)
+			{
+				Current = null;
+			}
+		}
+	}
+}
