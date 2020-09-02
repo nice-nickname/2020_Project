11bf80c0d9de9661db76bb82924f6bbbc59d9ab3
demon <marsinyo228@mail.ru>
2020-09-02 21:01:55 +0300

    добавил меню

---
+++ Project/Assets/Scripts/Menu/FullorWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullorWindow : MonoBehaviour
+{
+    public Toggle toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.fullScreen = true;
+
+        toggle.isOn = false;
+    }
+
+    public void ScreenMode()
+    {
+        Screen.fullScreen = !toggle.isOn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
